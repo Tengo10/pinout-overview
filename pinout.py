94517bc2cfc071acc092b0e931d314552aad44ec
Max <tengo10@web.de>
2023-05-09 23:22:22 +0200

    started work on Diagonal QFN some small refactoring

--- pinout.py
+++ pinout.py
@@ -1,4 +1,8 @@
 import drawsvg as dw
+import math
+
+SIN_45 = 0.7071067811865475
+COS_45 = 0.7071067811865476
 
 class Package:
     def __init__(self, name, footprint):
@@ -12,6 +16,7 @@ class Package:
         self.pin_length = 0
         self.pin_width = 0
         self.corner_spacing = 0
+        self.is_diag = False
 
         self.label_width = 0
         self.label_spacing = 0
@@ -33,8 +38,6 @@ class Package:
             "description": "spacer"
         }
 
-        # self.dwPinout = dw.Drawing(2000, 1000, origin='center', displayInline=False)
-
     def set_footprint(self, pin_number, pin_spacing, pin_length, pin_width, corner_spacing):
         self.pin_number = pin_number
         self.pin_spacing = pin_spacing
@@ -53,108 +56,208 @@ class Package:
     def add_pins(self, pins):
         self.pins = pins
 
-    def _generate_footprint(self):
-        dw_footprint = dw.Group(id=self.name)
-        if self.footprint == "QFN":
-            self.package_width = package_width = (self.pin_number/4-1) * self.pin_spacing + 2*self.corner_spacing
-            self.package_height = self.package_width
-            outer = dw.Rectangle(0, 0, package_width, package_width, 
+    def __draw_QFN(self):
+
+        pin_length = self.pin_length
+        package_width = self.package_width
+
+        footprint = dw.Group(id=self.name, transform=f"rotate({45 if self.is_diag else 0}, {package_width/2}, {package_width/2})")
+
+        border = dw.Rectangle(0, 0, package_width, package_width, 
                                  stroke="black", stroke_width=2, fill="grey")
-            
-            dw_footprint.append(outer)
+        
 
-            pad = dw.Path(stroke_width=2, stroke='black', fill='lightgrey')
-            pad.M(self.corner_spacing+self.pin_width, self.corner_spacing-self.pin_width/2)
-            pad.H(package_width-self.corner_spacing+self.pin_width/2)
-            pad.V(package_width-self.corner_spacing+self.pin_width/2)
-            pad.H(self.corner_spacing-self.pin_width/2)
-            pad.V(self.corner_spacing+self.pin_width)
-            pad.Z()
+        pad = dw.Path(stroke_width=2, stroke='black', fill='lightgrey')
+        pad.M(self.corner_spacing+self.pin_width, self.corner_spacing-self.pin_width/2)
+        pad.H(package_width-self.corner_spacing+self.pin_width/2)
+        pad.V(package_width-self.corner_spacing+self.pin_width/2)
+        pad.H(self.corner_spacing-self.pin_width/2)
+        pad.V(self.corner_spacing+self.pin_width)
+        pad.Z()
 
-            dw_pins = dw.Group(id="Pins")
-            pin = dw.Rectangle(0, 0,
+        pins = dw.Group(id="Pins")
+        pin = dw.Rectangle(0, 0,
                                self.pin_width, self.pin_length,
                                stroke="black", stroke_width=2, fill="lightgrey")
-            
-            for p in range(int(self.pin_number/4)):
-                dw_pins.append(dw.Use(pin, self.pin_spacing*p, 0,))
-
-            for i in range(4):
-                dw_footprint.append(dw.Use(dw_pins, self.corner_spacing-self.pin_width/2, 0,
-                                            transform=f"rotate({i*90}, {package_width/2}, {package_width/2})"))
-
-            point = dw.Circle(self.corner_spacing-self.pin_width/4, self.corner_spacing-self.pin_width/4,
-                              self.pin_width/4, stroke="black", stroke_width=2, fill="lightgrey")
-            
-
-            dw_footprint.append(point)
-            
-            dw_footprint.append(pad)
-            
-
-
-        elif self.footprint == "SOP":
-            self.package_height = (self.pin_number/2+1) * self.pin_spacing
-            self.package_width = 200
-            print(self.package_height)
-            #outside = dw.Rectangle(self.pin_length,0, self.package_width, self.package_height, 
-            #                       stroke="black", stroke_width=2, fill="none")
-            #inside = dw.Rectangle(self.pin_length+self.package_width*0.025, self.package_width*0.025, 
-            #                      self.package_width-self.package_width*0.025*2, self.package_height-self.package_width*0.025*2, 
-            #                      stroke="black", stroke_width=2, fill="none")
-            
-            border = dw.Path(stroke="black", stroke_width=2, fill="none")
-            border.M(self.pin_length, 0)
-            border.H(self.pin_length+self.package_width)
-            border.V(self.package_height)
-            border.H(self.pin_length)
-            border.V(0)
-            border.M(self.pin_length+self.package_width*0.025, self.package_width*0.025)
-            border.H(self.pin_length+self.package_width*(1-0.025))
-            border.V(self.package_height-self.package_width*0.025)
-            border.H(self.pin_length+self.package_width*0.025)
-            border.V(self.package_width*0.025)
-            border.L(self.pin_length, 0)
-            border.M(self.pin_length+self.package_width, 0)
-            border.L(self.pin_length+self.package_width*(1-0.025), self.package_width*0.025)
-            border.M(self.pin_length+self.package_width, self.package_height)
-            border.L(self.pin_length+self.package_width*(1-0.025), self.package_height-self.package_width*0.025)
-            border.M(self.pin_length, self.package_height)
-            border.L(self.pin_length+self.package_width*0.025, self.package_height-self.package_width*0.025)
-            border.Z()
-            #border.M(self.pac)
-
-
-            dw_pin = dw.Path(stroke="black", stroke_width=2, fill="none")
-            dw_pin.M(0,0)
-            dw_pin.H(self.pin_length)
-            dw_pin.V(self.pin_width)
-            dw_pin.H(0)
-            dw_pin.V(0)
-            dw_pin.M(self.pin_length/3, 0)
-            dw_pin.V(self.pin_width)
-            dw_pin.M(self.pin_length*2/3, 0)
-            dw_pin.V(self.pin_width)
-            dw_pin.Z()
-
-            dw_pins = dw.Group(id="Pins")
-
-            for p in range(int(self.pin_number/2)):
-                dw_pins.append(dw.Use(dw_pin, 0, self.pin_spacing*(p+1)))
-                dw_pins.append(dw.Use(dw_pin, self.pin_length+self.package_width, self.pin_spacing*(p+1)))
-
-            #dw_footprint.append(outside)
-            #dw_footprint.append(inside)
-            dw_footprint.append(border)
-            dw_footprint.append(dw_pins)
-
+        
+        for p in range(int(self.pin_number/4)):
+                pins.append(dw.Use(pin, self.pin_spacing*p, 0,))
+        
+        dot = dw.Circle(self.corner_spacing-self.pin_width/6, self.corner_spacing-self.pin_width/6,
+                              self.pin_width/3, stroke="black", stroke_width=2, fill="lightgrey")
+        
         footprint_text = dw.Text(f"{self.footprint}-{self.pin_number}", 30, 0, 0,
                                      text_anchor='middle', dominant_baseline='middle',
                                      fill="black", font_weight='bold', font_family='Roboto Mono')
         
-        dw_footprint.append(dw.Use(footprint_text, self.package_width/2, self.package_height/2))
-        return dw_footprint
+               
+        footprint.append(border)
+        footprint.append(pad)
+        footprint.append(dot)
+        for i in range(4):
+            footprint.append(dw.Use(pins, self.corner_spacing-self.pin_width/2, 0,
+                                    transform=f"rotate({i*90}, {package_width/2}, {package_width/2})"))
+        footprint.append(dw.Use(footprint_text, self.package_width/2, self.package_height/2))
+        
+        return footprint
+
+    def __draw_SOP(self):
+        footprint = dw.Group(id=self.name)
+
+        border = dw.Path(stroke="black", stroke_width=2, fill="none")
+        border.M(self.pin_length, 0)
+        border.H(self.pin_length+self.package_width)
+        border.V(self.package_height)
+        border.H(self.pin_length)
+        border.V(0)
+        border.M(self.pin_length+self.package_width*0.025, self.package_width*0.025)
+        border.H(self.pin_length+self.package_width*(1-0.025))
+        border.V(self.package_height-self.package_width*0.025)
+        border.H(self.pin_length+self.package_width*0.025)
+        border.V(self.package_width*0.025)
+        border.L(self.pin_length, 0)
+        border.M(self.pin_length+self.package_width, 0)
+        border.L(self.pin_length+self.package_width*(1-0.025), self.package_width*0.025)
+        border.M(self.pin_length+self.package_width, self.package_height)
+        border.L(self.pin_length+self.package_width*(1-0.025), self.package_height-self.package_width*0.025)
+        border.M(self.pin_length, self.package_height)
+        border.L(self.pin_length+self.package_width*0.025, self.package_height-self.package_width*0.025)
+        border.Z()
+        
+        pin = dw.Path(stroke="black", stroke_width=2, fill="none")
+        pin.M(0,0)
+        pin.H(self.pin_length)
+        pin.V(self.pin_width)
+        pin.H(0)
+        pin.V(0)
+        pin.M(self.pin_length/3, 0)
+        pin.V(self.pin_width)
+        pin.M(self.pin_length*2/3, 0)
+        pin.V(self.pin_width)
+        pin.Z()
+
+        pins = dw.Group(id="Pins")
+
+        for p in range(int(self.pin_number/2)):
+            pins.append(dw.Use(pin, 0, self.pin_spacing*(p+1)))
+            pins.append(dw.Use(pin, self.pin_length+self.package_width, self.pin_spacing*(p+1)))
+
+        footprint.append(border)
+        footprint.append(pins)
+
+        return footprint
+        
+    def __calc_index_QFN(self):
+        label_pos_index = []
+        # Left side
+        for i in range(int(self.pin_number/4)):
+            index = [-1,
+                        [-self.package_width/2, self.corner_spacing + i*self.pin_spacing-self.package_height/2],
+                        [-self.label_start-self.package_width/2, self.corner_spacing + i*self.pin_spacing-self.package_height/2]]
+            label_pos_index.append(index)
+        extraSide = 0
+        if self.pin_number/4 % 2 != 0:
+            extraSide = 1
+        # Bottom Left
+        for i in range(int(self.pin_number/8)+extraSide):
+            index = [-1, 
+                        [self.corner_spacing + i*self.pin_spacing-self.package_width/2, self.package_width/2],
+                        [-self.label_start-self.package_width/2,2*self.corner_spacing + (i+1.5)*self.pin_spacing]]
+            label_pos_index.append(index)
+        # Bottom Right
+        for i in range(int(self.pin_number/8)):
+            i2 = int(self.pin_number/4) + int(self.pin_number/8) - i - 1 + extraSide
+            i3 = int(self.pin_number/8) + extraSide + i
+            index = [1,
+                        [self.corner_spacing + i3*self.pin_spacing-self.package_width/2, self.package_width/2],
+                        [self.package_width/2 + self.label_start, 2*self.corner_spacing + i2*self.pin_spacing-self.package_height/2]]
+            label_pos_index.append(index)
+        # Right side
+        for i in range(int(self.pin_number/4)):
+            i2 = int(self.pin_number/4) - i - 1
+            index = [1, 
+                        [self.package_width/2, self.corner_spacing + i2*self.pin_spacing-self.package_height/2],
+                        [self.package_width/2 + self.label_start, self.corner_spacing + i2*self.pin_spacing-self.package_height/2]]
+            label_pos_index.append(index)
+        # Top Right
+        for i in range(int(self.pin_number/8)):
+            i2 = int(self.pin_number/4) - i - 1
+            index = [1,
+                        [self.corner_spacing + i2*self.pin_spacing-self.package_width/2, -self.package_height/2],
+                        [self.package_width + self.label_start-self.package_width/2, -self.corner_spacing - (i+extraSide)*self.pin_spacing-self.package_height/2]]
+            label_pos_index.append(index)
+        # Top Left
+        for i in range(int(self.pin_number/8)+extraSide):
+            i2 = int(self.pin_number/4) - int(self.pin_number/8) - i -1
+            i3 = int(self.pin_number/8) + extraSide - i -1
+            index = [-1,
+                        [self.corner_spacing + i2*self.pin_spacing-self.package_width/2, -self.package_height/2],
+                        [-self.label_start-self.package_width/2, -self.corner_spacing - i3*self.pin_spacing-self.package_height/2]]
+            label_pos_index.append(index)
+        return label_pos_index
     
+    def __calc_index_QFN_diag(self):
+        label_pos_index = []
+        # Left Top side
+        top_start_y = - self.package_width * SIN_45 + self.corner_spacing * SIN_45# - self.pin_width/2 * SIN_45
+        top_start_x = - self.corner_spacing * COS_45# - self.pin_width/2 * COS_45
+
+        middle_start_x = (self.corner_spacing + self.pin_spacing*4)*SIN_45
+        middle_start_y = self.corner_spacing * COS_45
+
+        label_spacing = self.pin_spacing / SIN_45
+
+        for i in range(int(self.pin_number/4)):
+            step = (self.pin_spacing)*SIN_45*i
+            index = [ -1,
+                    [top_start_x - step, top_start_y + step],
+                    [top_start_x - (self.pin_spacing)*SIN_45*4-50, top_start_y + step*1.4-label_spacing]
+                 ]
+            label_pos_index.append(index)
+        # Left Bottom side
+        for i in range(int(self.pin_number/4)):
+            step = (self.pin_spacing)*SIN_45*i
+            index = [ -1,
+                    [-middle_start_x + step, middle_start_y + step],
+                    [top_start_x - (self.pin_spacing)*SIN_45*4-50, middle_start_y + step*1.4+label_spacing]
+                 ]
+            label_pos_index.append(index)
+
+        # Right Top side
+        for i in range(int(self.pin_number/4)):
+            step = (self.pin_spacing)*SIN_45*i
+            index = [ 1,
+                    [-top_start_x + step, top_start_y + step],
+                    [-top_start_x + step + 100, top_start_y + step*1.4-label_spacing]
+                 ]
+            label_pos_index.append(index)
+        
+        # Right Bottom side
+        for i in range(int(self.pin_number/4)):
+            step = (self.pin_spacing)*SIN_45*i
+            index = [ 1,
+                    [middle_start_x - step, middle_start_y + step],
+                    [middle_start_x - step + 100, middle_start_y + step*1.4+label_spacing]
+                 ]
+            label_pos_index.append(index)
+        return label_pos_index
+    
+
+    def _generate_footprint(self):
+        dw_footprint = dw.Group(id=self.name)
+
+        if self.footprint == "QFN":
+            self.package_width  = (self.pin_number/4-1) * self.pin_spacing + 2*self.corner_spacing
+            self.package_height = self.package_width        
+            dw_footprint = self.__draw_QFN()
+        elif self.footprint == "SOP":
+            self.package_height = (self.pin_number/2+1) * self.pin_spacing
+            self.package_width = self.package_height/2
+
+            dw_footprint = self.__draw_SOP()
+
+        return dw_footprint
+
     def __label_box(self, width, height, borderColour, borderWidth, backgroundColour, skew, radius):
         label_box = dw.Rectangle(0, 0, width, height, rx=radius, ry=radius, 
                                  stroke=borderColour, stroke_width=borderWidth, 
@@ -211,57 +314,17 @@ class Package:
         return dw_label_line
 
     def _generate_pinout(self):
+        
         dw_labels = dw.Group(id="Labels")
         dw_footprint = self._generate_footprint()
+        self._calculate_size()
         self.label_height = self.pin_spacing-10
         label_pos_index = []
-        if self.footprint == "QFN":
-            # Left side
-            for i in range(int(self.pin_number/4)):
-                index = [-1, 
-                         [0, self.corner_spacing + i*self.pin_spacing],
-                         [-self.label_start, self.corner_spacing + i*self.pin_spacing]]
-                label_pos_index.append(index)
-            extraSide = 0
-            if self.pin_number/4 % 2 != 0:
-                extraSide = 1
-            # Bottom Left
-            for i in range(int(self.pin_number/8)+extraSide):
-                i2 = i + int(self.pin_number/4)
-                index = [-1, 
-                         [self.corner_spacing + i*self.pin_spacing, self.package_width],
-                         [-self.label_start,2*self.corner_spacing + i2*self.pin_spacing]]
-                label_pos_index.append(index)
-            # Bottom Right
-            for i in range(int(self.pin_number/8)):
-                i2 = int(self.pin_number/4) + int(self.pin_number/8) - i - 1 + extraSide
-                i3 = int(self.pin_number/8) + extraSide + i
-                index = [1,
-                         [self.corner_spacing + i3*self.pin_spacing, self.package_width],
-                         [self.package_width + self.label_start, 2*self.corner_spacing + i2*self.pin_spacing]]
-                label_pos_index.append(index)
-            # Right side
-            for i in range(int(self.pin_number/4)):
-                i2 = int(self.pin_number/4) - i - 1
-                index = [1, 
-                         [self.package_width, self.corner_spacing + i2*self.pin_spacing],
-                         [self.package_width + self.label_start, self.corner_spacing + i2*self.pin_spacing]]
-                label_pos_index.append(index)
-            # Top Right
-            for i in range(int(self.pin_number/8)):
-                i2 = int(self.pin_number/4) - i - 1
-                index = [1,
-                         [self.corner_spacing + i2*self.pin_spacing, 0],
-                         [self.package_width + self.label_start, -self.corner_spacing - (i+extraSide)*self.pin_spacing]]
-                label_pos_index.append(index)
-            # Top Left
-            for i in range(int(self.pin_number/8)+extraSide):
-                i2 = int(self.pin_number/4) - int(self.pin_number/8) - i -1
-                i3 = int(self.pin_number/8) + extraSide - i -1
-                index = [-1,
-                         [self.corner_spacing + i2*self.pin_spacing, 0],
-                         [-self.label_start, -self.corner_spacing - i3*self.pin_spacing]]
-                label_pos_index.append(index)
+        if self.footprint == "QFN" and not self.is_diag:
+            label_pos_index = self.__calc_index_QFN()
+
+        elif self.footprint == "QFN" and self.is_diag:
+            label_pos_index = self.__calc_index_QFN_diag()
 
         elif self.footprint == "SOP":
             for i in range(int(self.pin_number/2)):
@@ -281,14 +344,15 @@ class Package:
             dw_labels.append(dw.Use(dw_pin, label_pos_index[i][2][0], label_pos_index[i][2][1]))
             dw_labels.append(self._generate_label_line(label_pos_index[i][1], label_pos_index[i][2]))
 
-        self._calculate_size()
+        
         self.dwPinout = dw.Drawing(self.canvas_width, self.canvas_height, origin='center', displayInline=True)
         print(self.canvas_width, "  ", self.canvas_height)
         self.dwPinout.embed_google_font("Roboto Mono")
         self.dwPinout.append(dw.Rectangle(-self.canvas_width/2, -self.canvas_height/2, self.canvas_width, self.canvas_height,
                                           stroke="black", stroke_width=2, fill="white"))
         self.dwPinout.append(dw.Use(dw_footprint, -self.package_width/2, -self.package_height/2))
-        self.dwPinout.append(dw.Use(dw_labels, -self.package_width/2, -self.package_height/2))
+        #self.dwPinout.append(dw.Use(dw_labels, -self.package_width/2, -self.package_height/2))
+        self.dwPinout.append(dw_labels)
         self.dwPinout.append(dw.Use(self._generate_legend(), -self.canvas_width/2, self.canvas_height/2-160))
         self.dwPinout.append(dw.Use(self._generate_title(), 0, -self.canvas_height/2+60))
 
